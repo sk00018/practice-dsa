303a5fffc6bf05b42eca37d35888370c9ef047bf
Sumit Kumar <ksumit1454@gmail.com>
2026-08-28 01:23:56 +0530

    binary trangle Pattern

--- pattern.cpp
+++ pattern.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int main(){
+int main() {
 
 // // Pattern 8: Pyramid Pattern  
 //     int n = 5;
@@ -26,25 +26,45 @@ int main(){
 
 
     
-// Pattern 9:Reverse star pyr. Pattern
+// // Pattern 9:Reverse star pyr. Pattern
 
+//     int n = 5;
+
+//     for (int i = 0; i < n; i++) {
+
+//           // Spaces
+//         for (int j = 0; j < i; j++) {
+//             cout << " ";
+//         }
+
+//         // Stars
+//         for (int j = 0; j < 2*(n-i); j++) {
+//             cout << "*";
+//         }
+
+//            // Spaces
+//         for (int j = 0; j < i; j++) {
+//             cout << " ";
+//         }
+
+//         cout << endl;
+//     }
+
+
+
+
+
+
+// // Pattern 11:binary trangle Pattern
     int n = 5;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
 
-          // Spaces
-        for (int j = 0; j < i; j++) {
-            cout << " ";
-        }
-
-        // Stars
-        for (int j = 0; j < 2*(n-i); j++) {
-            cout << "*";
-        }
-
-           // Spaces
-        for (int j = 0; j < i; j++) {
-            cout << " ";
+        for (int j = 1; j <= i; j++) {
+            if ((i + j) % 2 == 0)
+                cout << "1 ";
+            else
+                cout << "0 ";
         }
 
         cout << endl;
