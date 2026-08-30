21c3f17ac0f4e4c54c390d60be2a18a4cf4b77fd
Sumit Kumar <ksumit1454@gmail.com>
2026-08-30 23:18:13 +0530

    number_palindrome_pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -55,20 +55,50 @@ int main() {
 
 
 
-// // Pattern 11:binary trangle Pattern
-    int n = 5;
+// // // Pattern 11:binary trangle Pattern
+//     int n = 5;
 
+//     for (int i = 1; i <= n; i++) {
+
+//         for (int j = 1; j <= i; j++) {
+//             if ((i + j) % 2 == 0)
+//                 cout << "1";
+//             else
+//                 cout << "0";
+//         }
+
+//         cout << endl;
+//     }
+
+
+
+
+    
+// Pattern 11:number_palindrome_pattern
+
+    int n = 4;
+    
     for (int i = 1; i <= n; i++) {
 
+          // num
         for (int j = 1; j <= i; j++) {
-            if ((i + j) % 2 == 0)
-                cout << "1 ";
-            else
-                cout << "0 ";
+            cout << j ;
+        }
+
+        // Space
+        for (int j = 1; j <= 2*(n-i); j++) {
+            cout << " ";
+        }
+
+           // num
+        for (int j = i; j >= 1; j--) {
+            cout << j ;
         }
 
         cout << endl;
+        
     }
+
 
 
     return 0;
